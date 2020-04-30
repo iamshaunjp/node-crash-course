f71e91caa43a72d6b9a7084e96e13620adfb0289
Shaun <shaun@thenetninja.co.uk>
2020-04-30 14:42:38 +0100

    lesson-6

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+// express app
+const app = express();
+
+// listen for requests
+app.listen(3000);
+
+app.get('/', (req, res) => {
+  // res.send('<p>home page</p>');
+  res.sendFile('./views/index.html', { root: __dirname });
+});
+
+app.get('/about', (req, res) => {
+  // res.send('<p>about page</p>');
+  res.sendFile('./views/about.html', { root: __dirname });
+});
+
+// redirects
+app.get('/about-us', (req, res) => {
+  res.redirect('/about');
+});
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).sendFile('./views/404.html', { root: __dirname });
+});
