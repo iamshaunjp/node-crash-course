e3a298c727043566fea5e3ced3a6ff5bc5e8d410
Shaun <shaun@thenetninja.co.uk>
2020-04-30 16:35:52 +0100

    lesson-8

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 
 // express app
 const app = express();
@@ -8,7 +9,29 @@ app.listen(3000);
 
 // register view engine
 app.set('view engine', 'ejs');
-// app.set('views', 'myviews');
+
+// middleware & static files
+app.use(express.static('public'));
+
+app.use((req, res, next) => {
+  console.log('new request made:');
+  console.log('host: ', req.hostname);
+  console.log('path: ', req.path);
+  console.log('method: ', req.method);
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('in the next middleware');
+  next();
+});
+
+app.use(morgan('dev'));
+
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
+});
 
 app.get('/', (req, res) => {
   const blogs = [
