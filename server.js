2b77752d192e207239455aff51e2acdc618219a0
Shaun <shaun@thenetninja.co.uk>
2020-04-30 14:15:39 +0100

    lesson-5

--- server.js
+++ server.js
@@ -1,26 +1,21 @@
 const http = require('http');
 const fs = require('fs');
+const _ = require('lodash');
 
 const server = http.createServer((req, res) => {
-  // console.log(req);
-  console.log(req.url);
+
+  // lodash
+  const num = _.random(0, 20);
+  console.log(num);
+
+  const greet = _.once(() => {
+    console.log('hello');
+  });
+  greet();
+  greet();
 
   // set header content type
   res.setHeader('Content-Type', 'text/html');
-
-  // res.write('<p>hello, ninjas</p>');
-  // res.write('<p>hello again, ninjas</p>');
-  // res.end();
-
-  // send html file
-  // fs.readFile('./views/index.html', (err, data) => {
-  //   if (err) {
-  //     console.log(err);
-  //     res.end();
-  //   }
-  //   //res.write(data);
-  //   res.end(data);
-  // });
 
   // routing
   let path = './views/';
