fbe01239767ad10f00e5fca9a0a5f566a4d703ca
chow's outlook <chow971@outlook.com>
2022-02-08 11:40:48 -0500

    feature

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 
 // express app
 const app = express();
@@ -6,22 +6,24 @@ const app = express();
 // listen for requests
 app.listen(3000);
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
+  //Automatic set Headers and statusCode
   // res.send('<p>home page</p>');
-  res.sendFile('./views/index.html', { root: __dirname });
+  res.sendFile("./views/index.html", { root: __dirname });
 });
 
-app.get('/about', (req, res) => {
+app.get("/about", (req, res) => {
   // res.send('<p>about page</p>');
-  res.sendFile('./views/about.html', { root: __dirname });
+  res.sendFile("./views/about.html", { root: __dirname });
 });
 
 // redirects
-app.get('/about-us', (req, res) => {
-  res.redirect('/about');
+app.get("/about-us", (req, res) => {
+  res.redirect("/about");
 });
 
 // 404 page
+// if got not match above, it will fire this function
 app.use((req, res) => {
-  res.status(404).sendFile('./views/404.html', { root: __dirname });
+  res.status(404).sendFile("./views/404.html", { root: __dirname });
 });
